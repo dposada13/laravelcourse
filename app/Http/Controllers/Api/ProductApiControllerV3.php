66829fe6965e9bdee3170f4237e925b6c333cddf
dposada13 <dposada13@gmail.com>
2024-09-18 15:24:00 -0500

    api

---
+++ app/Http/Controllers/Api/ProductApiControllerV3.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductCollection;
+use App\Models\Product;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ProductApiControllerV3 extends Controller
+{
+    public function index(): JsonResponse
+    {
+        $products = new ProductCollection(Product::all());
+        return response()->json($products, 200);
+    }
+
+    public function paginate(): JsonResponse
+    {
+        $products = new ProductCollection(Product::paginate(5));
+        return response()->json($products, 200);
+    }
+
+    public function store(Request $request)
+    {
+        // Validar los datos
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'price' => 'required|numeric',
+        ]);
+
+        // Crear el producto
+        $product = Product::create([
+            'price' => $request->price,
+            'name' => $request->name,
+        ]);
+
+        return response()->json([
+            'message' => 'Producto creado exitosamente',
+            'product' => $product
+        ], 201);
+    }
+
+
+}
